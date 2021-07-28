f3f4f8cb94c3f0182719e6bb67b1842555095a13
Makus_Frig <72632895+MakusFrig@users.noreply.github.com>
2021-07-28 11:29:25 -0700

    Update main.py
    Made some updates to the command line version

--- code/main.py
+++ code/main.py
@@ -4,8 +4,18 @@ import json
 
 import sys
 
-TITLE = '''
+import os
 
+import string
+
+def clear_screen():
+	if sys.platform == "linux":
+		os.system("clear")
+	elif sys.platform == 'win32':
+		os.system('cls')
+clear_screen()
+
+TITLE = '''
    _____ _                               
   / ____| |                    _     _   
  | |    | |__   ___ _ __ ___ _| |_ _| |_ 
@@ -16,6 +26,27 @@ TITLE = '''
 Welcom to Chem++ Console Version 
 Type Help for a list of Commands
 '''
+
+THINGS = [
+        "AtomicNumber",
+        "Symbol",
+        "Name",
+        "AtomicMass",
+        "CPKHexColor",
+        "ElectronConfiguration",
+        "Electronegativity",
+        "AtomicRadius",
+        "IonizationEnergy",
+        "ElectronAffinity",
+        "OxidationStates",
+        "StandardState",
+        "MeltingPoint",
+        "BoilingPoint",
+        "Density",
+        "GroupBlock",
+        "YearDiscovered"
+      ]
+
 
 print(TITLE)
 
@@ -30,22 +61,54 @@ elif sys.platform == 'win32':
 for i in temp:
 	TABLE[i['Cell'][2]] = i['Cell']
 	
+def info(x):
+	print(f"\n\t+---Info on {x}---")
+	for e in TABLE:
+		if x == e:
+			for i in range(len(TABLE[e])):
+
+				if TABLE[e][i] == '':
+					print(f"\t|{i} = Unknown")
+
+				else:
+					print(f"\t|{THINGS[i]} = {TABLE[e][i]}")
+	print("\t+-----------" + "-"*len(x) + "---\n")
+
 def command(x):
+	global RUNNING
 	if x == "Exit":
+
 		RUNNING = False
+
+	elif x == "Help":
+		to_print = '''
+Help: Displays list of Commands
+
+Exit: Exits Chem++'''
+
+		print(to_print)
+
+	elif x[:4] == "Info":
+		info(x[5:])
+
+	elif x == "Cls" or x == 'cls':
+		clear_screen()
+
 
 RUNNING = True
 
 while RUNNING:
-	print("}")
+	print("```", end = '')
 	
-	CMD = input(" ")
+	CMD = str(input(""))
 	
 	command(CMD)
 	
 	
 	
-print("Exiting Chem++")
+print("\nExiting Chem++")
+
+clear_screen()
 	
 	
 	
